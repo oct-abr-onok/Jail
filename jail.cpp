01146f2b1a26dfae5cbff917c4c9bdbc06e1e3cc
oct_abr_onok <oct.abr.onok@gmail.com>
2022-10-28 14:12:45 +0700

    Added illustration app and skeleton code

---
+++ jail.cpp
@@ -0,0 +1,6 @@
+#include "jail.hpp"
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
---
+++ jail.hpp
@@ -0,0 +1,6 @@
+#ifndef JAIL_642
+#define JAIL_642
+#include <iostream>
+#include <vector>
+
+#endif /*JAIL_642*/
