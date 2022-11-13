910ef409d08d7f80256705d8ecfc37539a0fea70
oct_abr_onok <oct.abr.onok@gmail.com>
2022-11-13 23:38:21 +0700

    added CMake, strategy file and some minor fixes

--- jail.hpp
+++ jail.hpp
@@ -5,4 +5,14 @@
 #include <string>
 #include <fstream>
 
+class Strategy
+{
+private:
+    int points = 0;
+
+public:
+    Strategy() = default;
+    ~Strategy() = default;
+};
+
 #endif /*JAIL_642*/
---
+++ strategy_class.cpp
@@ -0,0 +1,2 @@
+#include "jail.hpp"
+
